4e0d07b7aa91aa50e22adbf60e5f830e2e1a3abb
gurjung <88628578+gurjung@users.noreply.github.com>
2026-07-21 16:21:19 +0530

    Time: 37 ms (66.96%), Space: 58.9 MB (82.81%) - LeetHub

--- 0322-coin-change/0322-coin-change.js
+++ 0322-coin-change/0322-coin-change.js
@@ -27,37 +27,53 @@ var coinChange = function (coins, amount) {
 
     // recursion + memo
 
+    // let n = coins.length;
+
+    // let dp = [];
+
+    // for (let i = 0; i <= amount; i++) {
+    //     dp[i] = [];
+    //     for (let j = 0; j <= n; j++) {
+    //         dp[i][j] = -1;
+    //     }
+    // }
+
+
+    // function recur(target, p) {
+    //     // base case
+    //     if (target === 0) return 0;
+
+    //     if (target < 0 || p >= n) {
+    //         return Infinity;
+    //     }
+
+    //     if (dp[target][p] !== -1) {
+    //         return dp[target][p]
+    //     }
+    //     // two choices 
+    //     let take = 1 + recur(target - coins[p], p);
+    //     let skip = recur(target, p + 1);
+    //     dp[target][p] = Math.min(take, skip)
+    //     return dp[target][p]
+
+    // }
+
+    // let ans = recur(amount, 0);
+    // return ans === Infinity ? -1 : ans
+
+    // tabulation
     let n = coins.length;
 
-    let dp = [];
+    let dp = new Array(amount + 1).fill(Infinity);
 
-    for (let i = 0; i <= amount; i++) {
-        dp[i] = [];
-        for (let j = 0; j <= n; j++) {
-            dp[i][j] = -1;
+    dp[0] = 0;
+
+    for (let i = 1; i <= amount; i++) {
+        for (let coin of coins) {
+            if (i >= coin) {
+                dp[i] = Math.min(dp[i], 1 + dp[i - coin])
+            }
         }
     }
-
-
-    function recur(target, p) {
-        // base case
-        if (target === 0) return 0;
-
-        if (target < 0 || p >= n) {
-            return Infinity;
-        }
-
-        if (dp[target][p] !== -1) {
-            return dp[target][p]
-        }
-        // two choices 
-        let take = 1 + recur(target - coins[p], p);
-        let skip = recur(target, p + 1);
-        dp[target][p] = Math.min(take, skip)
-        return dp[target][p]
-
-    }
-
-    let ans = recur(amount, 0);
-    return ans === Infinity ? -1 : ans
+    return dp[amount] === Infinity ? -1 : dp[amount]
 };
