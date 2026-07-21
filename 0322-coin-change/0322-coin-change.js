afde5037b36fd2eb4984a536b071fa9f46d9fa62
gurjung <88628578+gurjung@users.noreply.github.com>
2026-07-21 16:16:38 +0530

    Time: 288 ms (5.02%), Space: 80.4 MB (5.02%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.js
@@ -0,0 +1,63 @@
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+var coinChange = function (coins, amount) {
+    // // recursion
+
+    // let n = coins.length;
+
+    // function recur(target, p) {
+    //     // base case
+    //     if (target === 0) return 0;
+
+    //     if (target < 0 || p >= n) {
+    //         return Infinity;
+    //     }
+    //     // two choices 
+    //     let take = 1 + recur(target - coins[p], p);
+    //     let skip = recur(target, p + 1);
+    //     return Math.min(take, skip)
+
+    // }
+
+    // let ans = recur(amount, 0);
+    // return ans === Infinity ? -1 : ans
+
+    // recursion + memo
+
+    let n = coins.length;
+
+    let dp = [];
+
+    for (let i = 0; i <= amount; i++) {
+        dp[i] = [];
+        for (let j = 0; j <= n; j++) {
+            dp[i][j] = -1;
+        }
+    }
+
+
+    function recur(target, p) {
+        // base case
+        if (target === 0) return 0;
+
+        if (target < 0 || p >= n) {
+            return Infinity;
+        }
+
+        if (dp[target][p] !== -1) {
+            return dp[target][p]
+        }
+        // two choices 
+        let take = 1 + recur(target - coins[p], p);
+        let skip = recur(target, p + 1);
+        dp[target][p] = Math.min(take, skip)
+        return dp[target][p]
+
+    }
+
+    let ans = recur(amount, 0);
+    return ans === Infinity ? -1 : ans
+};
