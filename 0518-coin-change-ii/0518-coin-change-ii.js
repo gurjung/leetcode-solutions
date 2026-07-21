a0e47779674f04ae96adbb6aceccc0e0cd57bd04
gurjung <88628578+gurjung@users.noreply.github.com>
2026-07-21 16:37:14 +0530

    Time: 141 ms (30.97%), Space: 88.4 MB (32.24%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.js
+++ 0518-coin-change-ii/0518-coin-change-ii.js
@@ -25,7 +25,41 @@ var change = function (amount, coins) {
     // return recur(amount, 0)
 
 
-    // recursion + memo
+    // // recursion + memo
+    // let n = coins.length;
+
+    // let dp = [];
+
+    // for (let i = 0; i <= amount; i++) {
+    //     dp[i] = [];
+    //     for (let j = 0; j <= n; j++) {
+    //         dp[i][j] = -1;
+    //     }
+    // }
+
+    // function recur(target, p) {
+    //     // base case
+    //     if (target === 0) return 1;
+
+    //     if (target < 0) return 0;
+
+    //     if (p === n) return 0;
+
+    //     if (dp[target][p] !== -1) {
+    //         return dp[target][p]
+    //     }
+
+    //     // two choices
+    //     let take = recur(target - coins[p], p);
+    //     let skip = recur(target, p + 1);
+    //     dp[target][p] = take + skip
+    //     return dp[target][p]
+
+    // }
+
+    // return recur(amount, 0)
+
+    // tabulation
     let n = coins.length;
 
     let dp = [];
@@ -36,26 +70,27 @@ var change = function (amount, coins) {
             dp[i][j] = -1;
         }
     }
-
-    function recur(target, p) {
-        // base case
-        if (target === 0) return 1;
-
-        if (target < 0) return 0;
-
-        if (p === n) return 0;
-
-        if (dp[target][p] !== -1) {
-            return dp[target][p]
-        }
-
-        // two choices
-        let take = recur(target - coins[p], p);
-        let skip = recur(target, p + 1);
-        dp[target][p] = take + skip
-        return dp[target][p]
-
+    //base case
+    for (let p = 0; p <= n; p++) {
+        dp[0][p] = 1;
     }
 
-    return recur(amount, 0)
+    for (let target = 1; target <= amount; target++) {
+        for (let p = n; p >= 0; p--) {
+            // two choices
+
+            if (p === n || target < 0) {
+                dp[target][p] = 0;
+                continue;
+            }
+            let take = 0;
+            if (target >= coins[p]) {
+                take = dp[target - coins[p]][p];
+            }
+            let skip = dp[target][p + 1];
+            dp[target][p] = take + skip
+        }
+    }
+
+    return dp[amount][0]
 };
