ac4427e18cf0b7c3a413bc6a6bec899bdbf8cc26
gurjung <88628578+gurjung@users.noreply.github.com>
2026-07-24 19:04:53 +0530

    Time: 65 ms (41.33%), Space: 83 MB (33.51%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.js
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.js
@@ -32,7 +32,49 @@ var longestCommonSubsequence = function (text1, text2) {
     // return recur(0, 0)
 
 
-    // recursion + memo
+    // // recursion + memo
+
+    // let m = text1.length;
+    // let n = text2.length;
+
+    // let dp = [];
+
+    // for (let i = 0; i <= m; i++) {
+    //     dp[i] = [];
+    //     for (let j = 0; j <= n; j++) {
+    //         dp[i][j] = -1;
+    //     }
+    // }
+
+    // function recur(i, j) {
+    //     // base case
+
+    //     if (i >= m || j >= n) {
+    //         return 0;
+    //     }
+
+    //     if (dp[i][j] !== -1) {
+    //         return dp[i][j]
+    //     }
+
+    //     if (text1[i] === text2[j]) {
+    //         // one choice
+    //         let path = 1 + recur(i + 1, j + 1);
+    //         dp[i][j] = path;
+    //         return dp[i][j];
+    //     } else {
+    //         // two choices
+    //         let path1 = recur(i + 1, j);
+    //         let path2 = recur(i, j + 1);
+    //         dp[i][j] = Math.max(path1, path2);
+    //         return dp[i][j]
+    //     }
+
+    // }
+
+    // return recur(0, 0)
+
+    // tabulation
 
     let m = text1.length;
     let n = text2.length;
@@ -46,31 +88,28 @@ var longestCommonSubsequence = function (text1, text2) {
         }
     }
 
-    function recur(i, j) {
-        // base case
+    // base case
 
-        if (i >= m || j >= n) {
-            return 0;
+
+    for (let i = m; i >= 0; i--) {
+        for (let j = n; j >= 0; j--) {
+            if (i === m || j === n) {
+                dp[i][j] = 0;
+                continue;
+            }
+
+            if (text1[i] === text2[j]) {
+                // one choice
+                dp[i][j] = 1 + dp[i + 1][j + 1];
+            } else {
+                // two choices
+                let path1 = dp[i + 1][j];
+                let path2 = dp[i][j + 1];
+                dp[i][j] = Math.max(path1, path2);
+            }
+
         }
-
-        if (dp[i][j] !== -1) {
-            return dp[i][j]
-        }
-
-        if (text1[i] === text2[j]) {
-            // one choice
-            let path = 1 + recur(i + 1, j + 1);
-            dp[i][j] = path;
-            return dp[i][j];
-        } else {
-            // two choices
-            let path1 = recur(i + 1, j);
-            let path2 = recur(i, j + 1);
-            dp[i][j] = Math.max(path1, path2);
-            return dp[i][j]
-        }
-
     }
 
-    return recur(0, 0)
+    return dp[0][0]
 };
