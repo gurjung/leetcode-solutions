709e31139dd4e403508d88ab5761d32bc1d70367
gurjung <88628578+gurjung@users.noreply.github.com>
2026-07-27 23:03:59 +0530

    Time: 38 ms (63.72%), Space: 81.3 MB (21.36%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
@@ -3,30 +3,51 @@
  * @return {number}
  */
 var longestConsecutive = function (nums) {
-    if (nums.length === 0) return 0;
+    // sorting approach -> nlogn
+    // if (nums.length === 0) return 0;
 
-    nums.sort((a, b) => a - b);
+    // nums.sort((a, b) => a - b);
 
-    let longest = 1;
-    let count = 1;
+    // let longest = 1;
+    // let count = 1;
 
-    for (let i = 1; i < nums.length; i++) {
+    // for (let i = 1; i < nums.length; i++) {
 
-        // Ignore duplicates
-        if (nums[i] === nums[i - 1]) {
-            continue;
-        }
+    //     // Ignore duplicates
+    //     if (nums[i] === nums[i - 1]) {
+    //         continue;
+    //     }
 
-        // Consecutive number
-        if (nums[i] === nums[i - 1] + 1) {
+    //     // Consecutive number
+    //     if (nums[i] === nums[i - 1] + 1) {
+    //         count++;
+    //     }
+    //     // Sequence broken
+    //     else {
+    //         count = 1;
+    //     }
+
+    //     longest = Math.max(longest, count);
+    // }
+
+    // return longest;
+
+    // hashset approach -> O(n)
+
+    let newSet = new Set(nums);
+
+    let longest = 0;
+
+    for (let num of newSet) {
+        if (newSet.has(num - 1)) continue;
+
+        let count = 1;
+        let currNum = num;
+        while (newSet.has(currNum + 1)) {
             count++;
+            currNum++;
         }
-        // Sequence broken
-        else {
-            count = 1;
-        }
-
-        longest = Math.max(longest, count);
+        longest = Math.max(longest, count)
     }
 
     return longest;
