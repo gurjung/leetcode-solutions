d1e84fcbdaa67c17e0342dddbde3ac1f95b88440
gurjung <88628578+gurjung@users.noreply.github.com>
2026-07-31 20:38:11 +0530

    Time: 879 ms (5.04%), Space: 67.3 MB (8.67%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.js
+++ 0621-task-scheduler/0621-task-scheduler.js
@@ -3,26 +3,149 @@
  * @param {number} n
  * @return {number}
  */
-var leastInterval = function (tasks, n) {
-    // Approach 1 -> 
-    
-    let freqArr = new Array(26).fill(0)
-    let maxFreq = 0;
-    for (let i = 0; i < tasks.length; i++) {
-        let curr = tasks[i].charCodeAt() - 65;
-        freqArr[curr]++;
-        maxFreq = Math.max(freqArr[curr], maxFreq)
+
+class MaxHeapOwn {
+    constructor() {
+        this.heap = []
     }
 
-    let countOfMaxFreqChars = 0;
+    getLeftChildIndex(i) {
+        return (2 * i) + 1;
+    }
 
-    for (let i = 0; i < 26; i++) {
-        if (freqArr[i] === maxFreq) {
-            countOfMaxFreqChars++;
+    getRightChildIndex(i) {
+        return (2 * i) + 2;
+    }
+
+    getParentIndex(i) {
+        return Math.floor((i - 1) / 2)
+    }
+
+    insert(val) {
+        this.heap.push(val);
+        let lastIndex = this.heap.length - 1;
+        this.heapifyUp(lastIndex);
+    }
+
+    heapifyUp(i) {
+        while (i > 0) {
+            let p = this.getParentIndex(i);
+            if (this.heap[p].val >= this.heap[i].val) {
+                break;
+            } else {
+                // swap
+                [this.heap[p], this.heap[i]] = [this.heap[i], this.heap[p]];
+                i = p;
+            }
         }
     }
 
-    let cycles = ((n + 1) * (maxFreq - 1)) + countOfMaxFreqChars;
+    extract() {
+        if (this.heap.length < 1) return null;
+        let max = this.heap[0];
+        let lastIdx = this.heap.length - 1;
+        // swap first with last element
+        // [a, b] = [b, a]
+        [this.heap[0], this.heap[lastIdx]] = [this.heap[lastIdx], this.heap[0]]
 
-    return Math.max(cycles, tasks.length)
+        this.heap.pop();
+        if (this.heap.length > 0) {
+            this.heapifyDown(0);
+        }
+        return max;
+    }
+
+    peek() {
+        if (this.heap.length) {
+            return this.heap[0];
+        } else {
+            return null
+        }
+    }
+
+    heapifyDown(i) {
+        let largest = i;
+        let left = this.getLeftChildIndex(i);
+        let right = this.getRightChildIndex(i);
+        let n = this.heap.length;
+
+        // check smallest and left and find new Smallest
+        if (left < n && this.heap[left].val > this.heap[largest].val) {
+            largest = left;
+        }
+
+        if (right < n && this.heap[right].val > this.heap[largest].val) {
+            largest = right;
+        }
+
+        // [a, b] = [b, a]
+        if (largest !== i) {
+            [this.heap[largest], this.heap[i]] = [this.heap[i], this.heap[largest]]
+            this.heapifyDown(largest);
+        }
+    }
+
+    size() {
+        return this.heap.length;
+    }
+}
+
+var leastInterval = function (tasks, n) {
+    // Approach 2 -> using heaps
+    let maxHeap = new MaxHeapOwn();
+
+    // create freq map and map for seating arrangement
+    let freq = {};
+    let allocationMap = {};
+
+    for (let i = 0; i < tasks.length; i++) {
+        let char = tasks[i];
+        if (freq[char]) {
+            freq[char] = freq[char] + 1;
+        } else {
+            freq[char] = 1;
+            allocationMap[char] = 1;
+        }
+    }
+
+    for (let key in freq) {
+        let obj = {
+            val: freq[key],
+            char: key
+        }
+        maxHeap.insert(obj)
+    }
+
+    let seat = 1;
+
+    while (maxHeap.size() > 0) {
+
+        let idleArr = [];
+
+        while (maxHeap.size() > 0) {
+            let curr = maxHeap.extract();
+            let val = curr.val;
+            let char = curr.char;
+            if (allocationMap[char] <= seat) {
+                if (val > 1) {
+                    maxHeap.insert({
+                        val: val - 1,
+                        char: char
+                    })
+                    allocationMap[char] = seat + n + 1; // next free seat available
+                }
+                break;
+            } else {
+                idleArr.push(curr)
+            }
+        }
+
+        for (let i = 0; i < idleArr.length; i++) {
+            maxHeap.insert(idleArr[i])
+        }
+        seat++;
+    }
+
+    return seat - 1;
+
 };
