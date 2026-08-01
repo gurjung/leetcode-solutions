5846a6746041075c5a36cc3f0e873cd43274d7ba
gurjung <88628578+gurjung@users.noreply.github.com>
2026-08-01 17:55:28 +0530

    Time: 19 ms (5.16%), Space: 60.1 MB (10.46%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.js
+++ 0057-insert-interval/0057-insert-interval.js
@@ -5,25 +5,64 @@
  */
 var insert = function (arr, x) {
     // Approach 1 -> 
-    let result = [];
-    // left non overlapping part
-    let i = 0;
-    while (i < arr.length && arr[i][1] < x[0]) {
-        result.push(arr[i]);
-        i++;
+    // let result = [];
+    // // left non overlapping part
+    // let i = 0;
+    // while (i < arr.length && arr[i][1] < x[0]) {
+    //     result.push(arr[i]);
+    //     i++;
+    // }
+
+    // // overlapping part
+    // while (i < arr.length && arr[i][0] <= x[1]) {
+    //     x[0] = Math.min(x[0], arr[i][0]);
+    //     x[1] = Math.max(x[1], arr[i][1]);
+    //     i++;
+    // }
+    // result.push(x);
+    // // right non overlapping part
+    // while (i < arr.length) {
+    //     result.push(arr[i])
+    //     i++;
+    // }
+    // return result;
+
+    // Appraoch 2 -> insert + merge
+
+    let newArr = []; // to hold new list (old list + new interval)
+    let insertedFlag = false;
+
+    for (let i = 0; i < arr.length; i++) {
+        let start = arr[i][0];
+        if (!insertedFlag && start >= x[0]) {
+            newArr.push(x);
+            insertedFlag = true;
+        }
+        newArr.push(arr[i]);
+    }
+    if (!insertedFlag) {
+        newArr.push(x);
     }
 
-    // overlapping part
-    while (i < arr.length && arr[i][0] <= x[1]) {
-        x[0] = Math.min(x[0], arr[i][0]);
-        x[1] = Math.max(x[1], arr[i][1]);
-        i++;
+    console.log(newArr, 'debug')
+
+    // now apply merge interval here on new list
+    // [ [ 1, 2 ], [ 3, 5 ], [ 4, 8 ], [ 6, 7 ], [ 8, 10 ], [ 12, 16 ] ]
+    let ans = [];
+    ans.push(newArr[0]);
+
+    for (let i = 1; i < newArr.length; i++) {
+        let start2 = newArr[i][0];
+        let end1 = ans[ans.length - 1][1];
+        if (start2 <= end1) {
+            let end2 = newArr[i][1];
+            ans[ans.length - 1][1] = Math.max(end1, end2);
+        } else {
+            ans.push(newArr[i])
+        }
+
     }
-    result.push(x);
-    // right non overlapping part
-    while (i < arr.length) {
-        result.push(arr[i])
-        i++;
-    }
-    return result;
+
+    return ans
+
 };
