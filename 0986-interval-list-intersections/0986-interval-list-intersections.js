3101ecda44b8a7c61ee7501745afec36412aa310
gurjung <88628578+gurjung@users.noreply.github.com>
2026-08-02 15:41:27 +0530

    Time: 3 ms (71.43%), Space: 64.6 MB (51.65%) - LeetHub

--- 0986-interval-list-intersections/0986-interval-list-intersections.js
+++ 0986-interval-list-intersections/0986-interval-list-intersections.js
@@ -4,7 +4,42 @@
  * @return {number[][]}
  */
 var intervalIntersection = function (firstList, secondList) {
-    // Approach -> 2 pointer with intervals
+    // // Approach -> 2 pointer with intervals
+    // let n = firstList.length;
+    // let m = secondList.length;
+    // let i = j = 0;
+
+    // let res = []
+
+    // while (i < n && j < m) {
+    //     let s1 = firstList[i][0]; // start1
+    //     let e1 = firstList[i][1];
+    //     let s2 = secondList[j][0];
+    //     let e2 = secondList[j][1];
+
+    //     // check for overlap condition
+    //     if (s1 <= s2) {
+    //         if (e1 >= s2) {
+    //             // overlapped, calc intersection points
+    //             res.push([Math.max(s1, s2), Math.min(e1, e2)])
+    //         }
+    //     } else {
+    //         if (e2 >= s1) {
+    //             // overlapped, calc intersection points
+    //             res.push([Math.max(s1, s2), Math.min(e1, e2)])
+    //         }
+    //     }
+
+    //     if (e1 <= e2) {
+    //         i++;
+    //     } else {
+    //         j++;
+    //     }
+    // }
+
+    // return res
+
+    // Approach 2 -> 2 pointer with simplified overlap logic
     let n = firstList.length;
     let m = secondList.length;
     let i = j = 0;
@@ -17,19 +52,16 @@ var intervalIntersection = function (firstList, secondList) {
         let s2 = secondList[j][0];
         let e2 = secondList[j][1];
 
-        // check for overlap condition
-        if (s1 <= s2) {
-            if (e1 >= s2) {
-                // overlapped, calc intersection points
-                res.push([Math.max(s1, s2), Math.min(e1, e2)])
-            }
-        } else {
-            if (e2 >= s1) {
-                // overlapped, calc intersection points
-                res.push([Math.max(s1, s2), Math.min(e1, e2)])
-            }
-        }
+        let start = Math.max(s1, s2);
+        let end = Math.min(e1, e2);
 
+
+        // check for overlap condition
+        if (start <= end) {
+
+            // overlapped, calc intersection points
+            res.push([start, end])
+        }
         if (e1 <= e2) {
             i++;
         } else {
@@ -37,5 +69,5 @@ var intervalIntersection = function (firstList, secondList) {
         }
     }
 
-    return res
-};
+    return res;
+}
