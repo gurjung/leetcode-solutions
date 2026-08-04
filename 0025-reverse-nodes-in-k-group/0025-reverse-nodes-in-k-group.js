bf1bc948af3efbd42286ee12257f7fdbed9f8dbf
gurjung <88628578+gurjung@users.noreply.github.com>
2026-08-04 20:54:44 +0530

    Time: 1 ms (50.53%), Space: 59 MB (67.11%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.js
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.js
@@ -12,28 +12,96 @@
  */
 var reverseKGroup = function (head, k) {
 
-    // Approach 1 -> recursion
+    // // Approach 1 -> recursion
 
-    // check whether k nodes exist or not
-    let temp = head, count = 0;
-    while (count < k) {
-        if (temp === null) {
-            return head;
+    // // check whether k nodes exist or not
+    // let temp = head, count = 0;
+    // while (count < k) {
+    //     if (temp === null) {
+    //         return head;
+    //     }
+    //     temp = temp.next;
+    //     count++;
+    // }
+    // // send temp as head in recursive call
+    // let prevNode = reverseKGroup(temp, k);
+    // // swap k group
+    // let curr = head;
+    // count = 0;
+    // while (count < k) {
+    //     let n = curr.next;
+    //     curr.next = prevNode;
+    //     prevNode = curr;
+    //     curr = n;
+    //     count++;
+    // }
+    // return prevNode;
+
+    // Approach 2 -> Iterative solution (same as swap nodes in pair solution, here size is k )
+
+    if (!head) return head;
+
+    let currLeft = head;
+    let size = k;
+    let currRight = null;
+    let nextLeft = null;
+    let prevLeft = null;
+    let ans = null;
+
+    function reverse(head, times) {
+        let curr = head;
+        let prev = null;
+        while (times > 0) {
+            let temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+            times--;
         }
-        temp = temp.next;
-        count++;
+        return;
     }
-    // send temp as head in recursive call
-    let prevNode = reverseKGroup(temp, k);
-    // swap k group
-    let curr = head;
-    count = 0;
-    while (count < k) {
-        let n = curr.next;
-        curr.next = prevNode;
-        prevNode = curr;
-        curr = n;
-        count++;
+
+    while (1) {
+        currRight = currLeft;
+
+        for (let i = 0; i < size - 1; i++) {
+            if (currRight === null) {
+                break;
+            }
+
+            currRight = currRight.next;
+        }
+
+        if (currRight) {
+            nextLeft = currRight.next; // saving for future left position
+
+            reverse(currLeft, size);
+
+            if (prevLeft) {
+                prevLeft.next = currRight;
+            }
+
+            prevLeft = currLeft; // store currLeft value in prevLeft for future use
+
+            currLeft = nextLeft;
+
+            if (ans === null) {
+                ans = currRight;
+            }
+
+        } else {
+            // everything ends now
+            if (prevLeft) {
+                prevLeft.next = currLeft; // because there is no currRight
+            }
+
+
+            if (ans === null) {
+                ans = currLeft; // if only node case then currLeft === currRight
+            }
+            break;
+        }
     }
-    return prevNode;
+
+    return ans;
 };
