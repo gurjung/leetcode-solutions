c133ab1cbdad0c4440936ae8ada99fe816c7ed4b
gurjung <88628578+gurjung@users.noreply.github.com>
2026-08-05 19:23:37 +0530

    Time: 40 ms (5.8%), Space: 67 MB (16.57%) - LeetHub

---
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.js
@@ -0,0 +1,57 @@
+/**
+ * @param {number[]} bloomDay
+ * @param {number} m
+ * @param {number} k
+ * @return {number}
+ */
+var minDays = function (arr, m, k) {
+    // m bouquets
+    // k flowers in each bouquet
+    // total flowers n
+    // 
+    // invalid case
+    let totalFlowersReq = m * k
+    if (arr.length < totalFlowersReq) return -1;
+
+    // bloomDay = [7,7,7,7,12,7,7], m = 2, k = 3
+
+    // low = 7;
+    // high = 12;
+    // mid = 12
+    // 1 + 1 + 1 + 1 + 1 + 1 + 1
+    let low = Math.min(...arr);
+    let high = Math.max(...arr);
+
+    const calculateFlowers = (days) => {
+        let sum = 0;
+        let ans = 0;
+        for (let i = 0; i < arr.length; i++) {
+            if (days >= arr[i]) {
+                sum = sum + 1;
+                if (sum % k === 0) {
+                    ans = ans + sum;
+                    sum = 0;
+                }
+            } else {
+                sum = 0;
+            }
+        }
+        console.log(ans, "ans")
+        return ans;
+    }
+
+    // let flowers = calculateFlowers(9);
+    let result = high;
+
+    while (low <= high) {
+        let mid = low + Math.floor((high - low) / 2);
+        let flowers = calculateFlowers(mid);
+        if (flowers >= totalFlowersReq) {
+            high = mid - 1;
+            result = Math.min(result, mid)
+        } else {
+            low = mid + 1;
+        }
+    }
+    return result
+};
