bf7b3ecdb8aa4c0c66e1d1188da47f79d1019450
gurjung <88628578+gurjung@users.noreply.github.com>
2026-08-07 16:14:41 +0530

    Time: 3 ms (60.15%), Space: 58.9 MB (86.26%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum = function (candidates, target) {
+    // 
+    let result = [];
+    let temp = [];
+    // temp = [2, 2, 3 ]
+    // remsum = 7-2 = 5 - 2 = 3 - 2 = 1 - 2 = -1
+    function recur(remSum, temp, p) {
+        // base condition
+        if (remSum === 0) {
+            result.push([...temp])
+            return;
+        }
+        if (remSum < 0) {
+            return;
+        }
+
+        // recursive step and process
+        for (let i = p; i < candidates.length; i++) {
+            if (candidates[i] > target) continue;
+            temp.push(candidates[i])
+            recur(remSum - candidates[i], temp, i)
+            temp.pop();
+        }
+
+    }
+
+    recur(target, temp, 0);
+    return result
+};
