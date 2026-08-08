dd46f6ad3837ced5674cbbe21d9f9b860d21c438
gurjung <88628578+gurjung@users.noreply.github.com>
2026-08-08 23:23:01 +0530

    Time: 0 ms (100%), Space: 61.6 MB (39.94%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.js
+++ 0338-counting-bits/0338-counting-bits.js
@@ -3,24 +3,42 @@
  * @return {number[]}
  */
 var countBits = function (n) {
-    // Approach 1 -> O(n log n)
+    // // Approach 1 -> O(n log n)
 
-    let ans = [];
+    // let ans = [];
 
-    function countOnes(num) {
-        let res = 0;
-        while (num > 0) {
-            if (num % 2 === 1) {
-                res++;
-            }
+    // function countOnes(num) {
+    //     let res = 0;
+    //     while (num > 0) {
+    //         if (num % 2 === 1) {
+    //             res++;
+    //         }
 
-            num = Math.floor(num / 2)
-        }
-        return res;
-    }
+    //         num = Math.floor(num / 2)
+    //     }
+    //     return res;
+    // }
+
+    // for (let i = 0; i <= n; i++) {
+    //     ans.push(countOnes(i))
+    // }
+
+    // return ans
+
+    // Approach 2 -> DP
+
+    // bits(i) = bits(i / 2) + lastBit
+
+    // i = 5 -> bits(2) + 1 => 1 + 1 => 2
+
+    // i / 2 => right shift by 1
+
+    // last bit => num & 1
+
+    let ans = new Array(n + 1).fill(0);
 
     for (let i = 0; i <= n; i++) {
-        ans.push(countOnes(i))
+        ans[i] = ans[i >> 1] + (i & 1)
     }
 
     return ans
