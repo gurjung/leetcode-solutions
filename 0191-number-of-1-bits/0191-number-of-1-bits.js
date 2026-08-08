5d0742b462517e393043742cbf1cac22d468944f
gurjung <88628578+gurjung@users.noreply.github.com>
2026-08-08 23:40:52 +0530

    Time: 0 ms (100%), Space: 54.6 MB (41.9%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.js
+++ 0191-number-of-1-bits/0191-number-of-1-bits.js
@@ -4,10 +4,17 @@
  */
 var hammingWeight = function (n) {
     // Brian Kernighan's Algorithm
-    let ans = 0;
-    while (n > 0) {
-        ans++
-        n = n & (n - 1)
+    // let ans = 0;
+    // while (n > 0) {
+    //     ans++
+    //     n = n & (n - 1)
+    // }
+    // return ans;
+
+    // Approach 2 ->
+    let result = 0;
+    for (let i = 0; i <= 31; i++) {
+        if ((1 << i) & n) result++;
     }
-    return ans
+    return result
 };
