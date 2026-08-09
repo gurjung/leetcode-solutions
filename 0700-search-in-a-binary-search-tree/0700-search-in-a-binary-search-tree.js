8fb16eb0baf18bdc9e40baf7bb58216b9ed2ecf8
gurjung <88628578+gurjung@users.noreply.github.com>
2026-08-09 17:41:31 +0530

    Time: 0 ms (100%), Space: 62.2 MB (44.41%) - LeetHub

--- 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.js
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.js
@@ -14,12 +14,27 @@
 var searchBST = function (root, val) {
     // approach 1 -> linear search
 
+    // function recur(node) {
+    //     if (!node) return null;
+    //     if (node.val === val) return node;
+    //     let l = recur(node.left);
+    //     let r = recur(node.right);
+    //     return l || r;
+    // }
+
+    // return recur(root);
+
+    // approach 2 -> Binary search
+
+
     function recur(node) {
         if (!node) return null;
         if (node.val === val) return node;
-        let l = recur(node.left);
-        let r = recur(node.right);
-        return l || r;
+        if(node.val > val) {
+            return recur(node.left);
+        } else {
+            return recur(node.right);
+        }
     }
 
     return recur(root);
