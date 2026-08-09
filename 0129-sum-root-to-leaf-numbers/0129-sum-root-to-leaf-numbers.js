ebaed799b1f3bd686ef89614ce6ff1f2256413fc
gurjung <88628578+gurjung@users.noreply.github.com>
2026-08-09 22:46:46 +0530

    Time: 0 ms (100%), Space: 51.9 MB (100%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.js
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.js
@@ -11,20 +11,42 @@
  * @return {number}
  */
 var sumNumbers = function (root) {
+    // let sum = 0;
+    // let temp = ""
+    // function recur(curr, temp) {
+    //     // base condition
+    //     if (!curr) return;
+
+    //     if (!curr.left && !curr.right) {
+    //         temp = temp + curr.val
+    //         sum = sum + Number(temp);
+    //         return;
+    //     }
+
+    //     recur(curr.left, temp + curr.val)
+    //     recur(curr.right, temp + curr.val)
+    // }
+
+    // recur(root, temp)
+    // return sum
+
+    // Approach 2 ->
+
     let sum = 0;
-    let temp = ""
+    let temp = 0;
     function recur(curr, temp) {
         // base condition
         if (!curr) return;
 
+        temp = temp * 10 + curr.val;
+
         if (!curr.left && !curr.right) {
-            temp = temp + curr.val
-            sum = sum + Number(temp);
+            sum = sum + temp
             return;
         }
 
-        recur(curr.left, temp + curr.val)
-        recur(curr.right, temp + curr.val)
+        recur(curr.left, temp)
+        recur(curr.right, temp)
     }
 
     recur(root, temp)
