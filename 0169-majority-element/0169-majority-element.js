1b4974b5a1228b0a08ce8397d11869e8024080f4
gurjung <88628578+gurjung@users.noreply.github.com>
2026-08-14 21:10:52 +0530

    Time: 0 ms (100%), Space: 57.7 MB (39.28%) - LeetHub

--- 0169-majority-element/0169-majority-element.js
+++ 0169-majority-element/0169-majority-element.js
@@ -2,12 +2,34 @@
  * @param {number[]} nums
  * @return {number}
  */
-var majorityElement = function(nums) {
+var majorityElement = function (nums) {
     // nums = [2,2,1,1,1,2,2]
 
     // Approach 1 -> use sorting and return middle element
 
-    nums.sort((a, b) => a - b);
+    // nums.sort((a, b) => a - b);
 
-    return nums[Math.floor(nums.length / 2)]
+    // return nums[Math.floor(nums.length / 2)]
+
+    // Approach 2 -> Moore's Voting Algorithm
+
+    let count = 0;
+    let candidate = null;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (count === 0) {
+            candidate = nums[i];
+        }
+
+        if (candidate !== nums[i]) {
+            count--;
+        }
+
+        if (candidate === nums[i]) {
+            count++;
+        }
+    }
+
+    return candidate;
+
 };
