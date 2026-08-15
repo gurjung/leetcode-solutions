8481d0b0a6d23e41e62333869765d51637f0a1f9
gurjung <88628578+gurjung@users.noreply.github.com>
2026-08-15 16:33:30 +0530

    Time: 0 ms (100%), Space: 52.5 MB (95.78%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
@@ -2,26 +2,23 @@
  * @param {number[]} nums
  * @return {number}
  */
-var findMin = function (arr) {
-    // need to check first occurence where it starts decreasing
-    // nums = [4,5,6,7,0,1,2]
-    // l = 4 => 
-    // r = 6 
-    // m = 5
-    // val = 1
-    let n = arr.length;
+var findMin = function (nums) {
+    let n = nums.length;
     let low = 0;
     let high = n - 1;
+    //low and high represents indexes
     let min = Infinity;
+
     while (low <= high) {
         let mid = low + Math.floor((high - low) / 2);
-        if (arr[mid] <= arr[n - 1]) {
-            min = mid;
-            high = mid - 1;
-        } else {
-            low = mid + 1;
-        }
 
+        if (nums[mid] <= nums[n - 1]) {
+            min = mid;
+            high = mid - 1; // make sure it is first occurence
+        } else {
+            low = mid + 1
+        }
     }
-    return arr[min]
+
+    return nums[min]
 };
