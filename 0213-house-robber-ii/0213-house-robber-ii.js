8fdbf5db75acdea3db6e73490bcef4179daf1913
gurjung <88628578+gurjung@users.noreply.github.com>
2026-08-15 20:25:59 +0530

    Time: 0 ms (100%), Space: 53.5 MB (60.32%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.js
+++ 0213-house-robber-ii/0213-house-robber-ii.js
@@ -3,11 +3,13 @@
  * @return {number}
  */
 var rob = function (nums) {
+
+    // [1,2,3,1,4]
     let n = nums.length;
 
-    if (n === 1) return nums[0];
+    if(n === 1) return nums[0]
 
-    function recur(start, end) {
+    function calculate(start, end) {
         let p1 = 0;
         let p2 = 0;
 
@@ -15,13 +17,13 @@ var rob = function (nums) {
             let rob = nums[i] + p2;
             let skip = p1;
             let curr = Math.max(rob, skip);
-
             p2 = p1;
             p1 = curr;
-
         }
-        return p1
+
+        return p1;
+
     }
 
-    return Math.max(recur(0, n - 2), recur(1, n - 1))
+    return Math.max(calculate(0, n - 2), calculate(1, n - 1))
 };
