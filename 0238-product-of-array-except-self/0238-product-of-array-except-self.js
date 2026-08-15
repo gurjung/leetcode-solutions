f6faa2e8d83c7418f892b8c4053787c86eea7f76
gurjung <88628578+gurjung@users.noreply.github.com>
2026-08-15 15:10:26 +0530

    Time: 36 ms (9.59%), Space: 73.6 MB (9.95%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -5,23 +5,53 @@
 var productExceptSelf = function (nums) {
     // Approach 1 -> with division
 
-    let totalProduct = nums.reduce((acc, curr) => acc * curr, 1);
-    let answer = [];
+    // let totalProduct = nums.reduce((acc, curr) => acc * curr, 1);
+    // let answer = [];
 
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] === 0) {
-            let product = 1;
-            for (let j = 0; j < nums.length; j++) {
-                if (i !== j) {
-                    product = product * nums[j]
-                }
-            }
-            answer.push(product)
-        } else {
-            let val = Math.floor(totalProduct / nums[i]);
-            answer.push(val)
-        }
+    // for (let i = 0; i < nums.length; i++) {
+    //     if (nums[i] === 0) {
+    //         let product = 1;
+    //         for (let j = 0; j < nums.length; j++) {
+    //             if (i !== j) {
+    //                 product = product * nums[j]
+    //             }
+    //         }
+    //         answer.push(product)
+    //     } else {
+    //         let val = Math.floor(totalProduct / nums[i]);
+    //         answer.push(val)
+    //     }
+    // }
+
+    // return answer;
+
+    // Approach 2 -> prefix and suffix (without division)
+    // [1,2,3,4]
+    // prefix -> [1, 1, 2, 6]
+    // suffix -> [24,12, 4, 1]
+    let n = nums.length;
+
+    let answer = new Array(n).fill(1);
+
+    let prefix = [];
+    let suffix = [];
+    prefix[0] = 1;
+    suffix[n - 1] = 1;
+
+    for (let i = 1; i < n; i++) {
+        prefix[i] = prefix[i - 1] * nums[i - 1]; // current value not required that is why nums[i - 1]
+    }
+
+    for (let i = n - 2; i >= 0; i--) {
+        suffix[i] = suffix[i + 1] * nums[i + 1]; // current value not required that is why nums[i + 1]
+    }
+
+    console.log(prefix, suffix, "DEBUG")
+
+    for (let i = 0; i < n; i++) {
+        answer[i] = prefix[i] * suffix[i]
     }
 
     return answer;
+
 };
