f71dac5e8f50281e64a84f7166b89a524fa8cda1
gurjung <88628578+gurjung@users.noreply.github.com>
2026-08-15 16:20:39 +0530

    Time: 0 ms (100%), Space: 64.7 MB (40.95%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.js
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.js
@@ -4,7 +4,9 @@
  */
 var peakIndexInMountainArray = function (arr) {
     let low = 0;
-    let high = arr.length;
+    let high = arr.length - 1;
+    // low and high => indexes
+
     let peak = 0;
 
     while (low <= high) {
@@ -13,10 +15,11 @@ var peakIndexInMountainArray = function (arr) {
         if (arr[mid] < arr[mid + 1]) {
             low = mid + 1; // climbing phase
         } else {
-            peak = mid; // Descending phase
-            high = mid - 1; // to make sure this is first occurence
+            peak = mid; 
+            high = mid - 1; // down phase and to make sure it is first occurence
         }
     }
 
     return peak
+
 };
