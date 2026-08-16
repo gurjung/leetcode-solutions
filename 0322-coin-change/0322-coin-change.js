a2437159ff1682d62a2cc7b2e3c2f258c8b8aa7a
gurjung <88628578+gurjung@users.noreply.github.com>
2026-08-17 01:46:20 +0530

    Time: 30 ms (86.11%), Space: 57.5 MB (97.57%) - LeetHub

--- 0322-coin-change/0322-coin-change.js
+++ 0322-coin-change/0322-coin-change.js
@@ -27,44 +27,63 @@ var coinChange = function (coins, amount) {
     // return ans === Infinity ? -1 : ans
 
 
-    // Approach 2 -> recursion + memo
+    // // Approach 2 -> recursion + memo
 
-    let n = coins.length;
+    // let n = coins.length;
 
-    let dp = []
+    // let dp = []
 
-    for (let i = 0; i < amount + 1; i++) {
-        dp[i] = [];
-        for (let j = 0; j < coins.length; j++) {
-            dp[i][j] = -1;
+    // for (let i = 0; i < amount + 1; i++) {
+    //     dp[i] = [];
+    //     for (let j = 0; j < coins.length; j++) {
+    //         dp[i][j] = -1;
+    //     }
+    // }
+    // for(let j = 0; j < coins.length; j++) {
+    //     dp[0][j] = 0;
+    // }
+
+
+    // function recur(remSum, p) {
+    //     if (remSum === 0) {
+    //         return 0;
+    //     }
+
+    //     if (remSum < 0 || p >= n) {
+    //         return Infinity;
+    //     }
+
+    //     if (dp[remSum][p] !== -1) {
+    //         return dp[remSum][p]
+    //     }
+
+    //     // two choices
+    //     let take = 1 + recur(remSum - coins[p], p)
+    //     let skip = recur(remSum, p + 1);
+
+    //     dp[remSum][p] = Math.min(take, skip);
+    //     return dp[remSum][p]
+    // }
+
+    // let ans = recur(amount, 0);
+    // return ans === Infinity ? -1 : ans
+
+    // Approach 3 -> tabulation
+
+    let dp = new Array(amount + 1).fill(Infinity);
+
+    // base case
+    dp[0] = 0;
+
+    for (let i = 1; i <= amount; i++) {
+        for (let coin of coins) {
+            if (i >= coin) {
+                dp[i] = Math.min(dp[i], 1 + dp[i - coin])
+            }
+
         }
     }
-    for(let j = 0; j < coins.length; j++) {
-        dp[0][j] = 0;
-    }
 
+    return dp[amount] === Infinity ? -1 : dp[amount]
 
-    function recur(remSum, p) {
-        if (remSum === 0) {
-            return 0;
-        }
-
-        if (remSum < 0 || p >= n) {
-            return Infinity;
-        }
-
-        if (dp[remSum][p] !== -1) {
-            return dp[remSum][p]
-        }
-
-        // two choices
-        let take = 1 + recur(remSum - coins[p], p)
-        let skip = recur(remSum, p + 1);
-
-        dp[remSum][p] = Math.min(take, skip);
-        return dp[remSum][p]
-    }
-
-    let ans = recur(amount, 0);
-    return ans === Infinity ? -1 : ans
 };
