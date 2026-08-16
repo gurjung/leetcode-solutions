4014d60e62a36e76c0fd276a5e18cc319acf8dcb
gurjung <88628578+gurjung@users.noreply.github.com>
2026-08-16 23:01:29 +0530

    Time: 42 ms (81.39%), Space: 53.7 MB (45.81%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.js
+++ 0509-fibonacci-number/0509-fibonacci-number.js
@@ -5,13 +5,35 @@
 var fib = function (n) {
     // approach 1 -> recursion
 
+    // function recur(num) {
+    //     // base condition
+    //     if (num === 0 || num === 1) {
+    //         return num
+    //     }
+
+    //     return recur(num - 1) + recur(num - 2)
+    // }
+
+    // return recur(n)
+
+    // approach 2 -> recursion + memo
+
+    let dp = new Array(n + 1).fill(-1);
+    dp[0] = 0;
+    dp[1] = 1;
+
     function recur(num) {
         // base condition
         if (num === 0 || num === 1) {
             return num
         }
 
-        return recur(num - 1) + recur(num - 2)
+        if (dp[num] !== -1) {
+            return dp[num]
+        }
+
+        dp[num] = recur(num - 1) + recur(num - 2)
+        return dp[num]
     }
 
     return recur(n)
