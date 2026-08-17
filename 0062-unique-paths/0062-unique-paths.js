6c9adcca436e18667c04c57ffa5b904b5484f5be
gurjung <88628578+gurjung@users.noreply.github.com>
2026-08-17 23:53:33 +0530

    Time: 3 ms (15.27%), Space: 54.8 MB (21.04%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.js
+++ 0062-unique-paths/0062-unique-paths.js
@@ -4,38 +4,65 @@
  * @return {number}
  */
 var uniquePaths = function (m, n) {
+
     // m = 3, n = 2
-    // // recursion
+    // o/p = 3
+
+    // Approach 1 -> recursion
+
+    // function isValid(r, c) {
+    //     if (r < 0 || r >= m || c < 0 || c >= n) {
+    //         return false;
+    //     }
+
+    //     return true;
+    // }
+
 
     // function recur(i, j) {
     //     // base case
-    //     if (j > n || i > m) return 0;
+
+    //     if (!isValid(i, j)) return 0;
 
     //     if (i === m - 1 && j === n - 1) {
     //         return 1;
     //     }
-    //     // two choices
-    //     // go right
+
     //     let right = recur(i, j + 1);
     //     let down = recur(i + 1, j);
-    //     return right + down
+
+    //     return right + down;
     // }
 
     // return recur(0, 0)
 
-    // recursion + memo
+    // Approach 2 -> recursion + memo
+
     let dp = [];
 
-    for (let i = 0; i <= m; i++) {
+    for (let i = 0; i < m; i++) {
         dp[i] = [];
-        for (let j = 0; j <= n; j++) {
+        for (let j = 0; j < n; j++) {
             dp[i][j] = -1;
+            if (i === m - 1 && j === n - 1) {
+                dp[i][j] = 1
+            }
         }
     }
 
+    function isValid(r, c) {
+        if (r < 0 || r >= m || c < 0 || c >= n) {
+            return false;
+        }
+
+        return true;
+    }
+
+
     function recur(i, j) {
         // base case
-        if (j > n || i > m) return 0;
+
+        if (!isValid(i, j)) return 0;
 
         if (i === m - 1 && j === n - 1) {
             return 1;
@@ -44,12 +71,12 @@ var uniquePaths = function (m, n) {
         if (dp[i][j] !== -1) {
             return dp[i][j]
         }
-        // two choices
-        // go right
+
         let right = recur(i, j + 1);
         let down = recur(i + 1, j);
+
         dp[i][j] = right + down;
-        return dp[i][j]
+        return dp[i][j];
     }
 
     return recur(0, 0)
