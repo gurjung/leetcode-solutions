1063c17681b2d3121684e3d2654418801edb0824
gurjung <88628578+gurjung@users.noreply.github.com>
2026-08-19 18:51:01 +0530

    Time: 953 ms (16.72%), Space: 115.4 MB (14.31%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.js
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.js
@@ -36,7 +36,56 @@ var canPartition = function (nums) {
 
     // return recur(target, 0);
 
-    // approach 2 -> recursion + memo
+    // // approach 2 -> recursion + memo
+
+    // let n = nums.length;
+
+    // let sum = nums.reduce((arr, curr) => arr + curr, 0);
+
+    // if (sum % 2 !== 0) return false;
+
+    // let target = sum / 2;
+
+    // let dp = [];
+
+    // for (let i = 0; i <= sum; i++) {
+    //     dp[i] = [];
+
+    //     for (let j = 0; j <= n; j++) {
+    //         dp[i][j] = -1
+    //     }
+    // }
+
+    // function recur(remSum, p) {
+    //     // base condition
+    //     if(remSum === 0) {
+    //         return true;
+    //     }
+
+    //     if(p >= nums.length) {
+    //         return false;
+    //     }
+
+    //     if(dp[remSum][p] !== -1) {
+    //         return dp[remSum][p];
+    //     }
+
+    //     // two choices
+
+    //     let skip = recur(remSum, p + 1);
+    //     let takeit = false;
+    //     if (nums[p] <= remSum) {
+    //         takeit = recur(remSum - nums[p], p + 1)
+    //     }
+
+    //     dp[remSum][p] = takeit || skip;
+    //     return dp[remSum][p];
+    // }
+
+    // return recur(target, 0);
+
+
+    // approach 3 -> tabulation
 
     let n = nums.length;
 
@@ -52,36 +101,30 @@ var canPartition = function (nums) {
         dp[i] = [];
 
         for (let j = 0; j <= n; j++) {
-            dp[i][j] = -1
+            dp[i][j] = false;
         }
     }
 
-    function recur(remSum, p) {
-        // base condition
-        if(remSum === 0) {
-            return true;
-        }
-
-        if(p >= nums.length) {
-            return false;
-        }
-        
-        if(dp[remSum][p] !== -1) {
-            return dp[remSum][p];
-        }
-
-        // two choices
-
-        let skip = recur(remSum, p + 1);
-        let takeit = false;
-        if (nums[p] <= remSum) {
-            takeit = recur(remSum - nums[p], p + 1)
-        }
-
-        dp[remSum][p] = takeit || skip;
-        return dp[remSum][p];
+    for(let j = 0; j <= n; j++) {
+        dp[0][j] = true;
     }
 
-    return recur(target, 0);
-    
+
+    for (let remSum = 1; remSum <= sum; remSum++) {
+        for (let p = n - 1; p >= 0; p--) {
+
+            let skip = dp[remSum][p + 1];
+
+            let takeit = false;
+
+            if (nums[p] <= remSum) {
+                takeit = dp[remSum - nums[p]][p + 1]
+            }
+
+            dp[remSum][p] = takeit || skip;
+        }
+    }
+
+    return dp[target][0];
+
 };
