83f6f6e63c5e25cd63ac733afb3fd58f72541521
gurjung <88628578+gurjung@users.noreply.github.com>
2026-08-19 18:45:32 +0530

    Time: 929 ms (16.72%), Space: 120.4 MB (13.66%) - LeetHub

---
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.js
@@ -0,0 +1,87 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canPartition = function (nums) {
+    // nums = [1,5,11,5]
+    // sum = 1 + 5 + 11 + 5 => 22 => 11
+    // approach 1 -> recursion
+
+    // let sum = nums.reduce((arr, curr) => arr + curr, 0);
+
+    // if (sum % 2 !== 0) return false;
+
+    // let target = sum / 2;
+
+    // function recur(remSum, p) {
+    //     // base condition
+    //     if(remSum === 0) {
+    //         return true;
+    //     }
+
+    //     if(p >= nums.length) {
+    //         return false;
+    //     }
+
+    //     // two choices
+
+    //     let skip = recur(remSum, p + 1);
+    //     let takeit = false;
+    //     if (nums[p] <= remSum) {
+    //         takeit = recur(remSum - nums[p], p + 1)
+    //     }
+
+    //     return takeit || skip
+    // }
+
+    // return recur(target, 0);
+
+    // approach 2 -> recursion + memo
+
+    let n = nums.length;
+
+    let sum = nums.reduce((arr, curr) => arr + curr, 0);
+
+    if (sum % 2 !== 0) return false;
+
+    let target = sum / 2;
+
+    let dp = [];
+
+    for (let i = 0; i <= sum; i++) {
+        dp[i] = [];
+
+        for (let j = 0; j <= n; j++) {
+            dp[i][j] = -1
+        }
+    }
+
+    function recur(remSum, p) {
+        // base condition
+        if(remSum === 0) {
+            return true;
+        }
+
+        if(p >= nums.length) {
+            return false;
+        }
+        
+        if(dp[remSum][p] !== -1) {
+            return dp[remSum][p];
+        }
+
+        // two choices
+
+        let skip = recur(remSum, p + 1);
+        let takeit = false;
+        if (nums[p] <= remSum) {
+            takeit = recur(remSum - nums[p], p + 1)
+        }
+
+        dp[remSum][p] = takeit || skip;
+        return dp[remSum][p];
+    }
+
+    return recur(target, 0);
+    
+};
