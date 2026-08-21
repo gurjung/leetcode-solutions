20db90962094f85ab55e30cfcb04641c64f499fc
gurjung <88628578+gurjung@users.noreply.github.com>
2026-08-21 17:31:17 +0530

    Time: 5 ms (91.97%), Space: 63 MB (9.77%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.js
+++ 0072-edit-distance/0072-edit-distance.js
@@ -42,7 +42,60 @@ var minDistance = function (word1, word2) {
 
     // return recur(0, 0);
 
-    // Approach 2 -> recursion + memo
+    // // Approach 2 -> recursion + memo
+
+    // let m = word1.length;
+    // let n = word2.length;
+
+    // let dp = [];
+
+    // for (let i = 0; i <= m; i++) {
+    //     dp[i] = [];
+    //     for (let j = 0; j <= n; j++) {
+    //         dp[i][j] = -1;
+    //     }
+    // }
+
+    // // word1 = "horse", word2 = "ros"
+    // function recur(i, j) {
+
+    //     // base case
+
+    //     if (i >= m) {
+    //         return n - j;
+    //     }
+
+    //     if (j >= n) {
+    //         return m - i;
+    //     }
+
+    //     if (dp[i][j] !== -1) {
+    //         return dp[i][j]
+    //     }
+
+    //     if (word1[i] !== word2[j]) {
+
+    //         // three choices
+    //         // insert
+    //         let insert = 1 + recur(i, j + 1);
+    //         // delete
+    //         let deleteOp = 1 + recur(i + 1, j);
+    //         // replace
+    //         let replace = 1 + recur(i + 1, j + 1);
+
+    //         dp[i][j] = Math.min(insert, deleteOp, replace);
+    //         return dp[i][j]
+
+    //     } else {
+    //         dp[i][j] = recur(i + 1, j + 1);
+    //         return dp[i][j]
+    //     }
+
+    // }
+
+    // return recur(0, 0)
+
+    // Approach 3 -> Tabulation
 
     let m = word1.length;
     let n = word2.length;
@@ -56,42 +109,33 @@ var minDistance = function (word1, word2) {
         }
     }
 
-    // word1 = "horse", word2 = "ros"
-    function recur(i, j) {
-
-        // base case
-
-        if (i >= m) {
-            return n - j;
-        }
-
-        if (j >= n) {
-            return m - i;
-        }
-
-        if (dp[i][j] !== -1) {
-            return dp[i][j]
-        }
-
-        if (word1[i] !== word2[j]) {
-
-            // three choices
-            // insert
-            let insert = 1 + recur(i, j + 1);
-            // delete
-            let deleteOp = 1 + recur(i + 1, j);
-            // replace
-            let replace = 1 + recur(i + 1, j + 1);
-
-            dp[i][j] = Math.min(insert, deleteOp, replace);
-            return dp[i][j]
-
-        } else {
-            dp[i][j] = recur(i + 1, j + 1);
-            return dp[i][j]
-        }
-
+    for (let j = 0; j <= n; j++) {
+        dp[m][j] = n - j;
     }
 
-    return recur(0, 0)
+    for (let i = 0; i <= m; i++) {
+        dp[i][n] = m - i;
+    }
+
+    for (let i = m - 1; i >= 0; i--) {
+        for (let j = n - 1; j >= 0; j--) {
+            if (word1[i] !== word2[j]) {
+
+                // three choices
+                // insert
+                let insert = 1 + dp[i][j + 1];
+                // delete
+                let deleteOp = 1 + dp[i + 1][j];
+                // replace
+                let replace = 1 + dp[i + 1][j + 1];
+
+                dp[i][j] = Math.min(insert, deleteOp, replace);
+
+            } else {
+                dp[i][j] = dp[i + 1][j + 1];
+            }
+        }
+    }
+
+    return dp[0][0]
 };
