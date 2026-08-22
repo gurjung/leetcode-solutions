7d5b154bb31a4e1643fb2b286ad830c8c2d96328
gurjung <88628578+gurjung@users.noreply.github.com>
2026-08-22 22:48:50 +0530

    Time: 57 ms (34.25%), Space: 83.4 MB (19.86%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.js
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.js
@@ -6,7 +6,51 @@
 var makeConnected = function (n, connections) {
     // Approach 1 -> DFS
 
+    // if (connections.length < n - 1) return -1;
+    // let visitedSet = new Set();
+    // // create adj list
+    // let map = {};
+
+    // for (let i = 0; i < n; i++) {
+    //     map[i] = []
+    // }
+
+    // for (let i = 0; i < connections.length; i++) {
+    //     let u = connections[i][0];
+    //     let v = connections[i][1];
+    //     map[u].push(v);
+    //     map[v].push(u);
+    // }
+
+    // function dfs(node) {
+    //     // explore neighbors of node
+    //     visitedSet.add(node);
+
+    //     for(let n of map[node]) {
+    //         if(!visitedSet.has(n)) {
+    //             dfs(n);
+    //         }
+    //     }
+    // }
+
+
+
+    // let components = 0;
+    // for (let i = 0; i < n; i++) {
+    //     if (!visitedSet.has(i)) {
+    //         dfs(i);
+    //         components++;
+    //     }
+
+    // }
+
+    // return components - 1;
+
+
+    // Approach 2 -> BFS
+
     if (connections.length < n - 1) return -1;
+
     let visitedSet = new Set();
     // create adj list
     let map = {};
@@ -22,23 +66,29 @@ var makeConnected = function (n, connections) {
         map[v].push(u);
     }
 
-    function dfs(node) {
+    function bfs(node) {
         // explore neighbors of node
+        let q = [node];
         visitedSet.add(node);
 
-        for(let n of map[node]) {
-            if(!visitedSet.has(n)) {
-                dfs(n);
+        while (q.length) {
+            let curr = q.shift();
+            
+            for (let n of map[curr]) {
+                if (!visitedSet.has(n)) {
+                    visitedSet.add(n);
+                    q.push(n);
+                }
             }
         }
+
     }
 
-    
-
     let components = 0;
+
     for (let i = 0; i < n; i++) {
         if (!visitedSet.has(i)) {
-            dfs(i);
+            bfs(i);
             components++;
         }
 
