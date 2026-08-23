07dcee824f1b0c6662a779af8ec02a6b9540dd96
gurjung <88628578+gurjung@users.noreply.github.com>
2026-08-23 16:41:07 +0530

    Time: 893 ms (5.96%), Space: 87.3 MB (8.82%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
@@ -3,10 +3,12 @@
  * @return {string}
  */
 var longestPalindrome = function (s) {
+    // Approach -> DP O(n2)
     let n = s.length;
-    // create dp array of size n * n with intial false value
-    let dp = [];
-    let ans = [0, 0];
+
+    let ans = [0, 0]; // store index start and end
+
+    let dp = []; // represents isPalindrome or not
 
     for (let i = 0; i <= n; i++) {
         dp[i] = [];
@@ -14,27 +16,32 @@ var longestPalindrome = function (s) {
             dp[i][j] = false;
         }
     }
-    // base case filling with length 1
+
+    // base case of length 1 string
     for (let i = 0; i < n; i++) {
+        // "abc" -> "a", "b", "c"
         dp[i][i] = true;
-        // ans++;
     }
 
-    // base case filling with length 2
-    for (let i = 0; i < n; i++) {
+    // base case of length 2 strings
+    for (let i = 0; i < n - 1; i++) {
+        // "abc" -> "ab", "bc"
         if (s[i] === s[i + 1]) {
             dp[i][i + 1] = true;
             ans = [i, i + 1]
         }
     }
 
-    // from length 3 -> 
-    for (let len = 3; len <= n; len++) {
-        for (let i = 0; i <= n - len; i++) {
-            let j = i + len - 1;
-            if (s[i] === s[j] && dp[i + 1][j - 1] === true) {
-                dp[i][j] = true;
-                ans = [i, j]
+    // raceacar
+
+    for (let i = 2; i < n; i++) {
+        for (let j = 0; j < n - i + 1; j++) {
+            let start = j;
+            let end = j + i;
+
+            if (s[start] === s[end] && dp[start + 1][end - 1] === true) {
+                dp[start][end] = true
+                ans = [start, end]
             }
         }
     }
