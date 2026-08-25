0d58e645846925cde0d74ce9a928eb04539513c2
gurjung <88628578+gurjung@users.noreply.github.com>
2026-08-25 18:01:11 +0530

    Time: 2 ms (65.77%), Space: 55.2 MB (38.46%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.js
+++ 0045-jump-game-ii/0045-jump-game-ii.js
@@ -3,25 +3,19 @@
  * @return {number}
  */
 var jump = function (nums) {
-    // Track how farthest we can go from current position
+    // approach 2 -> 
     let farthest = 0;
     let jumps = 0;
     let n = nums.length;
     let currEnd = 0;
-
-    if (n === 1) {
-        return 0;
-    }
 
     for (let i = 0; i < n - 1; i++) {
         farthest = Math.max(farthest, i + nums[i]);
         if (i === currEnd) {
             jumps++;
             currEnd = farthest;
-
-            if (currEnd >= n - 1) {
-                return jumps
-            }
         }
     }
+
+    return jumps;
 };
