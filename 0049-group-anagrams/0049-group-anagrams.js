d4048fedf915461cf2e01d87a6deed5f00d8c997
gurjung <88628578+gurjung@users.noreply.github.com>
2026-08-28 19:47:11 +0530

    Time: 112 ms (5.02%), Space: 77.1 MB (5.05%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -4,20 +4,61 @@
  */
 var groupAnagrams = function (strs) {
     // approach 1 -> sort the individual strings 
+    // let map = {};
+
+    // // {"aet" : ['eat', 'tea', 'ate']}
+
+    // for (let i = 0; i < strs.length; i++) {
+    //     let str = strs[i];
+    //     let sortedStr = [...strs[i]].sort().join("");
+
+    //     if (map[sortedStr]) {
+    //         let val = map[sortedStr];
+    //         val.push(str);
+    //         map[sortedStr] = val
+    //     } else {
+    //         map[sortedStr] = [str];
+    //     }
+    // }
+
+    // return [...Object.values(map)]
+
+    // approach 2 -> frequency categorization 
     let map = {};
 
-    // {"aet" : ['eat', 'tea', 'ate']}
+    // {"a1e1t1" : ['eat', 'tea', 'ate']}
+    function calcFreq(x) {
+        // "eat"
+        let map = {};
+        for (let k = 0; k < x.length; k++) {
+            let char = x[k]
+            if (map[char]) {
+                map[char] = map[char] + 1
+            } else {
+                map[char] = 1
+            }
+        }
+        // input =  {e: 1, a: 1, t: 1}
+        // output = "a1e1t1"
+        let result = Object.keys(map).sort().map(key => `${key}${map[key]}`).join("");
+        // 1. Object.keys(map) = ["e", "a", "t"]
+        // 2. Object.keys(map).sort() = ["a", "e", "t"]
+        // 3. Object.keys(map).sort().map(key => `${key}${map[key]}`) = ["a1", "e1", "t1"]
+        // 4. Object.keys(map).sort().map(key => `${key}${map[key]}`).join("") = "a1e1t1"
+        return result;
+    }
 
     for (let i = 0; i < strs.length; i++) {
         let str = strs[i];
-        let sortedStr = [...strs[i]].sort().join("");
+        let newStr = calcFreq(str)
 
-        if (map[sortedStr]) {
-            let val = map[sortedStr];
+
+        if (map[newStr]) {
+            let val = map[newStr];
             val.push(str);
-            map[sortedStr] = val
+            map[newStr] = val
         } else {
-            map[sortedStr] = [str];
+            map[newStr] = [str];
         }
     }
 
