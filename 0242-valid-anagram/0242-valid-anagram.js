7e1dac685634258d28b9afec0b7729aea7d0c206
gurjung <88628578+gurjung@users.noreply.github.com>
2026-08-28 19:19:17 +0530

    Time: 14 ms (58.83%), Space: 56.1 MB (66.98%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -0,0 +1,35 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function (s, t) {
+    // approach -> frequency counter
+    if (s.length !== t.length) return false;
+
+    let map = {};
+
+
+    // calculating freq of string s
+    for (let i = 0; i < s.length; i++) {
+        let char = s[i];
+        if (map[char]) {
+            map[char] = map[char] + 1;
+        } else {
+            map[char] = 1;
+        }
+    }
+
+    for (let i = 0; i < t.length; i++) {
+        let char = t[i];
+        if (map[char]) {
+            map[char] = map[char] - 1
+        }
+    }
+
+    for (let key in map) {
+        let val = map[key];
+        if (val > 0) return false;
+    }
+    return true;
+};
