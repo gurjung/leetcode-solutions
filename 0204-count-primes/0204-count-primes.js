9284d062cf82337e3e235629b5328d75a047fd8b
gurjung <88628578+gurjung@users.noreply.github.com>
2026-08-29 00:33:27 +0530

    Time: 1342 ms (9%), Space: 93.5 MB (81.75%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countPrimes = function (n) {
+    // Approach -> sieve of eratosthenes
+    if (n < 2) return 0;
+
+    let isPrime = new Array(n).fill(true);
+    isPrime[0] = false;
+    isPrime[1] = false;
+
+    for (let i = 2; i * i < n; i++) {
+        if (isPrime[i]) {
+            // cancel all multiples of i
+            for (let j = i * i; j < n; j = j + i) {
+                isPrime[j] = false;
+            }
+        }
+    }
+
+    let count = 0;
+    for (let i = 2; i < n; i++) {
+        if (isPrime[i]) {
+            count++;
+        }
+    }
+
+    return count;
+};
