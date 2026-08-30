9d27150fdbe4bebba6f37ec91ff1218710de111c
gurjung <88628578+gurjung@users.noreply.github.com>
2026-08-30 23:41:49 +0530

    Time: 9 ms (86.39%), Space: 63.2 MB (33.92%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.js
@@ -0,0 +1,42 @@
+/**
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {boolean}
+ */
+var canFinish = function (numCourses, prerequisites) {
+    // kahn's algo
+    let map = {};
+    let indegreesArr = new Array(numCourses).fill(0);
+    let q = [];
+    let result = [];
+
+    // create adj list
+    for (let i = 0; i < numCourses; i++) {
+        map[i] = [];
+    }
+
+    for (let i = 0; i < prerequisites.length; i++) {
+        let [a, b] = prerequisites[i];
+        map[b].push(a);
+        indegreesArr[a] = indegreesArr[a] + 1;
+    }
+
+    for (let i = 0; i < numCourses; i++) {
+        if (indegreesArr[i] === 0) {
+            q.push(i)
+        }
+    }
+
+    while (q.length) {
+        let curr = q.shift();
+        result.push(curr);
+        for (let n of map[curr]) {
+            indegreesArr[n]--;
+            if (indegreesArr[n] === 0) {
+                q.push(n)
+            }
+        }
+    }
+
+    return result.length === numCourses
+};
