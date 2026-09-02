970f39e9181b9f2fd1e4e05de466da56c4ab1250
gurjung <88628578+gurjung@users.noreply.github.com>
2026-09-02 19:28:45 +0530

    Time: 238 ms (91.7%), Space: 53.9 MB (78.69%) - LeetHub

---
+++ 0079-word-search/0079-word-search.js
@@ -0,0 +1,53 @@
+/**
+ * @param {character[][]} board
+ * @param {string} word
+ * @return {boolean}
+ */
+var exist = function (board, word) {
+    let m = board.length;
+    let n = board[0].length;
+
+    let ans = false;
+
+    function recur(x, y, count) {
+        // base case
+        if (word.length === count) {
+            ans = true;
+        }
+
+        //recursive step and process
+        let original = board[x][y];
+        board[x][y] = '#'; // because i already seen it
+
+        // check all directions now
+        // top
+        if (y > 0 && board[x][y - 1] === word[count]) {
+            recur(x, y - 1, count + 1)
+        }
+        //left
+        if (x > 0 && board[x - 1][y] === word[count]) {
+            recur(x - 1, y, count + 1)
+        }
+        //right
+        if (x < m - 1 && board[x + 1][y] === word[count]) {
+            recur(x + 1, y, count + 1)
+        }
+        // bottom
+        if (y < n - 1 && board[x][y + 1] === word[count]) {
+            recur(x, y + 1, count + 1)
+        }
+
+        board[x][y] = original;
+    }
+
+
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (board[i][j] === word[0]) {
+                recur(i, j, 1)
+            }
+        }
+    }
+
+    return ans
+};
