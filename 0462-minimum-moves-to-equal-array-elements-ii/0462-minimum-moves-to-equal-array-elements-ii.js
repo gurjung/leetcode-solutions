52c0a23d34b83bf31decc659e2dcdbdbe06174d1
gurjung <88628578+gurjung@users.noreply.github.com>
2026-09-05 18:28:48 +0530

    Time: 7 ms (28.95%), Space: 57.8 MB (10.53%) - LeetHub

--- 0462-minimum-moves-to-equal-array-elements-ii/0462-minimum-moves-to-equal-array-elements-ii.js
+++ 0462-minimum-moves-to-equal-array-elements-ii/0462-minimum-moves-to-equal-array-elements-ii.js
@@ -3,17 +3,31 @@
  * @return {number}
  */
 var minMoves2 = function (arr) {
-    let minCost = Infinity;
+    // let minCost = Infinity;
 
-    for (let h = 0; h < arr.length; h++) {
-        let currentCost = 0;
+    // for (let h = 0; h < arr.length; h++) {
+    //     let currentCost = 0;
 
-        for (let i = 0; i < arr.length; i++) {
-            currentCost += Math.abs(arr[i] - arr[h]);
-        }
-        
-        minCost = Math.min(minCost, currentCost);
+    //     for (let i = 0; i < arr.length; i++) {
+    //         currentCost += Math.abs(arr[i] - arr[h]);
+    //     }
+
+    //     minCost = Math.min(minCost, currentCost);
+    // }
+
+    // return minCost;
+
+    // Approach 2 -> use median because everything getting close to median will reduce cost
+    arr.sort((a, b) => a - b);
+
+    let medianIdx = Math.floor(arr.length / 2)
+
+    let moves = 0;
+
+    for (const num of arr) {
+        moves = moves + Math.abs(num - arr[medianIdx]);
     }
 
-    return minCost;
+    return moves;
+
 };
