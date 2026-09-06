bfcf2fc686b3b521d610a787de6f1e0ee82b263c
gurjung <88628578+gurjung@users.noreply.github.com>
2026-09-07 00:57:35 +0530

    Time: 38 ms (8.29%), Space: 60.7 MB (20.24%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.js
+++ 0684-redundant-connection/0684-redundant-connection.js
@@ -3,7 +3,45 @@
  * @return {number[]}
  */
 var findRedundantConnection = function (edges) {
-    // Approach 1 -> dfs/bfs
+    // // Approach 1 -> dfs/bfs
+
+    // let visitedSet = new Set();
+    // let V = edges.length;
+
+    // let map = {};
+
+    // for (let i = 0; i < V; i++) {
+    //     map[i + 1] = []
+    // }
+
+    // function recur(node, target) {
+    //     if (node === target) return true;
+
+    //     visitedSet.add(node);
+
+    //     for (let n of (map[node] || [])) {
+    //         if (!visitedSet.has(n)) {
+    //             if (recur(n, target)) {
+    //                 return true;
+    //             }
+    //         }
+    //     }
+    //     return false;
+    // }
+
+    // for (let i = 0; i < V; i++) {
+    //     let [u, v] = edges[i];
+    //     visitedSet = new Set();
+    //     if (recur(u, v)) {
+    //         return [u, v]
+    //     }
+
+    //     map[u].push(v);
+    //     map[v].push(u);
+    // }
+
+
+    // Approach 2 -> bfs
 
     let visitedSet = new Set();
     let V = edges.length;
@@ -14,29 +52,42 @@ var findRedundantConnection = function (edges) {
         map[i + 1] = []
     }
 
-    function recur(node, target) {
-        if (node === target) return true;
-
-        visitedSet.add(node);
-
-        for (let n of (map[node] || [])) {
-            if (!visitedSet.has(n)) {
-                if (recur(n, target)) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
+    let q = [];
 
     for (let i = 0; i < V; i++) {
-        let [u, v] = edges[i];
+        // explore edge wise
+        let [u, v] = edges[i]
         visitedSet = new Set();
-        if (recur(u, v)) {
+
+        q = [u];
+
+        visitedSet.add(u);
+
+        let found = false;
+
+        while (q.length) {
+            let curr = q.shift();
+
+            if (curr === v) {
+                found = true;
+                break;
+            }
+
+            for (let n of (map[curr] || [])) {
+                if (!visitedSet.has(n)) {
+                    q.push(n);
+                    visitedSet.add(n);
+                }
+
+            }
+        }
+
+        if (found) {
             return [u, v]
         }
 
         map[u].push(v);
         map[v].push(u);
     }
+
 };
