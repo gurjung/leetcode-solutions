611eab02976e6ce734548ceeecb89e7e29f6d304
gurjung <88628578+gurjung@users.noreply.github.com>
2026-09-12 18:33:35 +0530

    Time: 0 ms (100%), Space: 54.4 MB (16.44%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function (nums, target) {
+    let low = 0;
+    let high = nums.length - 1;
+    let ans = nums.length;
+
+    while (low <= high) {
+        let mid = low + Math.floor((high - low) / 2);
+
+        if (nums[mid] >= target) {
+            ans = mid;
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+
+    return ans;
+};
