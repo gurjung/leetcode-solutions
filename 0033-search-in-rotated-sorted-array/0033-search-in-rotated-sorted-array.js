7513964b9872671141f778238beddca393864078
gurjung <88628578+gurjung@users.noreply.github.com>
2026-09-13 15:31:03 +0530

    Time: 0 ms (100%), Space: 55.4 MB (6.38%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -4,46 +4,74 @@
  * @return {number}
  */
 var search = function (nums, target) {
+    // let n = nums.length;
+    // let low = 0;
+    // let high = n - 1;
+    // // nums = [4,5,6,7,0,1,2]
+    // // l = 4, h = 4, m = 4
+    // let min = -1;
+    // // let minIdx = n;
+    // while (low <= high) {
+    //     let mid = low + Math.floor((high - low) / 2);
+    //     if (nums[mid] === target) {
+    //         return mid;
+    //     }
+    //     if (nums[mid] > nums[n - 1]) {
+    //         low = mid + 1
+    //     } else {
+    //         // minIdx = mid;
+    //         min = mid
+    //         high = mid - 1;
+    //     }
+    // }
+
+    // if (target >= nums[min] && target <= nums[n - 1] ) {
+    //     low = min;
+    //     high = n - 1;
+    // } else {
+    //     low = 0;
+    //     high = min - 1;
+    // }
+    // // normal bs
+    // while (low <= high) {
+    //     let mid = low + Math.floor((high - low) / 2);
+    //     if (nums[mid] === target) {
+    //         return mid;
+    //     }
+    //     if (nums[mid] > target) {
+    //         high = mid - 1;
+    //     } else {
+    //         low = mid + 1;
+    //     }
+    // }
+
+    // return -1;
+
+    // Approach 2 -> 
+
     let n = nums.length;
     let low = 0;
     let high = n - 1;
-    // nums = [4,5,6,7,0,1,2]
-    // l = 4, h = 4, m = 4
-    let min = -1;
-    // let minIdx = n;
+
     while (low <= high) {
         let mid = low + Math.floor((high - low) / 2);
-        if (nums[mid] === target) {
-            return mid;
+        if (nums[mid] === target) return mid
+        // Left half is sorted
+        if (nums[low] <= nums[mid]) {
+            if (nums[low] <= target && target < nums[mid]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
         }
-        if (nums[mid] > nums[n - 1]) {
-            low = mid + 1
-        } else {
-            // minIdx = mid;
-            min = mid
-            high = mid - 1;
-        }
-    }
-
-    if (target >= nums[min] && target <= nums[n - 1] ) {
-        low = min;
-        high = n - 1;
-    } else {
-        low = 0;
-        high = min - 1;
-    }
-    // normal bs
-    while (low <= high) {
-        let mid = low + Math.floor((high - low) / 2);
-        if (nums[mid] === target) {
-            return mid;
-        }
-        if (nums[mid] > target) {
-            high = mid - 1;
-        } else {
-            low = mid + 1;
+        // Right half is sorted
+        else {
+            if (nums[mid] < target && target <= nums[high]) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
         }
     }
-
     return -1;
 };
