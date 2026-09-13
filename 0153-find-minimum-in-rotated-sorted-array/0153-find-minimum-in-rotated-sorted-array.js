e92d8952aaaa8d9f26bca9824c65722c7b578902
gurjung <88628578+gurjung@users.noreply.github.com>
2026-09-13 14:32:26 +0530

    Time: 0 ms (100%), Space: 54 MB (28.96%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
@@ -6,19 +6,18 @@ var findMin = function (nums) {
     let n = nums.length;
     let low = 0;
     let high = n - 1;
-    //low and high represents indexes
+    // nums = [4,5,6,7,0,1,2]
+    // l = 4, h = 4, m = 4
     let min = Infinity;
-
     while (low <= high) {
         let mid = low + Math.floor((high - low) / 2);
-
-        if (nums[mid] <= nums[n - 1]) {
-            min = mid;
-            high = mid - 1; // make sure it is first occurence
-        } else {
+        if (nums[mid] > nums[n - 1]) {
             low = mid + 1
+        } else {
+            min = Math.min(min, nums[mid]);
+            high = mid - 1;
         }
     }
 
-    return nums[min]
+    return min
 };
