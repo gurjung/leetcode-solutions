b5f9990ddf03dac04ec546fbfee73c4358755ccd
gurjung <88628578+gurjung@users.noreply.github.com>
2026-09-14 17:04:12 +0530

    Time: 111 ms (21.93%), Space: 88.9 MB (11.94%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.js
@@ -0,0 +1,47 @@
+
+var MinStack = function () {
+    this.stack = [];
+};
+
+/** 
+ * @param {number} value
+ * @return {void}
+ */
+MinStack.prototype.push = function (value) {
+    let minVal = value;
+    if (this.stack.length > 0) {
+        let prevMinVal = this.stack[this.stack.length - 1][1];
+        minVal = Math.min(minVal, prevMinVal)
+    }
+    this.stack.push([value, minVal]);
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function () {
+    return this.stack.pop()
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function () {
+    return this.stack[this.stack.length - 1][0]
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function () {
+    return this.stack[this.stack.length - 1][1]
+};
+
+/** 
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(value)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
