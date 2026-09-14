2a0009f2f22e53ff01812b3e49268343f519a2ba
gurjung <88628578+gurjung@users.noreply.github.com>
2026-09-14 18:20:16 +0530

    Time: 0 ms (100%), Space: 54 MB (13.04%) - LeetHub

--- 0836-rectangle-overlap/0836-rectangle-overlap.js
+++ 0836-rectangle-overlap/0836-rectangle-overlap.js
@@ -4,20 +4,33 @@
  * @return {boolean}
  */
 var isRectangleOverlap = function (rec1, rec2) {
-    let xAxisRange1 = [rec1[0], rec1[2]]; // [0,2]
-    let yAxisRange1 = [rec1[1], rec1[3]]; // [0,2]
+    // let xAxisRange1 = [rec1[0], rec1[2]]; // [0,2]
+    // let yAxisRange1 = [rec1[1], rec1[3]]; // [0,2]
 
-    let xAxisRange2 = [rec2[0], rec2[2]]; // [1,3] 
-    let yAxisRange2 = [rec2[1], rec2[3]]; // [1,3]
+    // let xAxisRange2 = [rec2[0], rec2[2]]; // [1,3] 
+    // let yAxisRange2 = [rec2[1], rec2[3]]; // [1,3]
 
-    let start1 = Math.max(xAxisRange1[0], xAxisRange2[0]);
-    let end1 = Math.min(xAxisRange1[1], xAxisRange2[1]);
+    // let start1 = Math.max(xAxisRange1[0], xAxisRange2[0]);
+    // let end1 = Math.min(xAxisRange1[1], xAxisRange2[1]);
 
-    let start2 = Math.max(yAxisRange1[0], yAxisRange2[0]);
-    let end2 = Math.min(yAxisRange1[1], yAxisRange2[1]);
+    // let start2 = Math.max(yAxisRange1[0], yAxisRange2[0]);
+    // let end2 = Math.min(yAxisRange1[1], yAxisRange2[1]);
 
-    let commonX = end1 - start1;
-    let commonY = end2 - start2;
+    // let commonX = end1 - start1;
+    // let commonY = end2 - start2;
 
-    return commonX > 0 && commonY > 0
+    // return commonX > 0 && commonY > 0
+
+    // Approach 2 ->
+
+    if (
+        rec1[2] <= rec2[0] ||  // rec1 is left of rec2
+        rec1[0] >= rec2[2] ||  // rec1 is right of rec2
+        rec1[3] <= rec2[1] ||  // rec1 is below rec2
+        rec1[1] >= rec2[3]     // rec1 is above rec2
+    ) {
+        return false;
+    }
+
+    return true;
 };
