5adcd9de06756cfe63eb8f3783c384ef28053704
gurjung <88628578+gurjung@users.noreply.github.com>
2026-09-16 16:03:44 +0530

    Time: 45 ms (84.7%), Space: 67.9 MB (73.61%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.js
+++ 0542-01-matrix/0542-01-matrix.js
@@ -3,43 +3,46 @@
  * @return {number[][]}
  */
 var updateMatrix = function (mat) {
-    // Approach -> Multi-source BFS
+    // multi source bfs
 
     let m = mat.length;
     let n = mat[0].length;
 
     let clone = mat.map((row) => [...row]);
 
+    let dx = [1, -1, 0, 0];
+    let dy = [0, 0, -1, 1];
+
     let visitedArr = [];
 
     for (let i = 0; i < m; i++) {
         visitedArr[i] = []
         for (let j = 0; j < n; j++) {
-            visitedArr[i][j] = false
+            visitedArr[i][j] = false;
         }
     }
 
 
-    let dx = [1, -1, 0, 0];
-    let dy = [0, 0, -1, 1];
-
     function isValid(i, j) {
-        if (i >= m || i < 0 || j >= n || j < 0) {
-            return false;
+        if (i < 0 || i >= m || j < 0 || j >= n) {
+            return false
         }
-        return true;
+
+        return true
     }
 
     let q = []
 
+
     for (let i = 0; i < m; i++) {
         for (let j = 0; j < n; j++) {
-            if (mat[i][j] === 0) {
-                q.push([i, j]) // push all 0's and spread from there
-                visitedArr[i][j] = true;
+            if (!visitedArr[i][j] && mat[i][j] === 0) {
+                q.push([i, j]);
+                visitedArr[i][j] = true
             }
         }
     }
+
 
     while (q.length) {
         let curr = q.shift();
@@ -51,12 +54,12 @@ var updateMatrix = function (mat) {
 
             if (isValid(row, col) && !visitedArr[row][col]) {
                 q.push([row, col]);
-                clone[row][col] = clone[r][c] + 1
+                clone[row][col] = clone[r][c] + 1;
                 visitedArr[row][col] = true;
             }
         }
-
     }
 
-    return clone;
+    return clone
+
 };
