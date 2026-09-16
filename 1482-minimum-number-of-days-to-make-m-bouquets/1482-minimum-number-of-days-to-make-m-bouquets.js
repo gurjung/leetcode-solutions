2344c2da01430793d61469ab3b223c2b1cdc0a4f
gurjung <88628578+gurjung@users.noreply.github.com>
2026-09-16 18:57:34 +0530

    Time: 19 ms (70.57%), Space: 66.4 MB (32.91%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.js
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.js
@@ -4,106 +4,51 @@
  * @param {number} k
  * @return {number}
  */
-var minDays = function (arr, m, k) {
-    // // Approach 1 -> by calculating flowers
-    // // m bouquets
-    // // k flowers in each bouquet
-    // // total flowers n
-    // // 
-    // // invalid case
-    // let totalFlowersReq = m * k
-    // if (arr.length < totalFlowersReq) return -1;
+var minDays = function (bloomDay, m, k) {
 
-    // // bloomDay = [7,7,7,7,12,7,7], m = 2, k = 3
+    let ans = Infinity;
 
-    // // low = 7;
-    // // high = 12;
-    // // mid = 12
-    // // 1 + 1 + 1 + 1 + 1 + 1 + 1
-    // let low = Math.min(...arr);
-    // let high = Math.max(...arr);
+    if (bloomDay.length < (m * k)) return -1;
 
-    // const calculateFlowers = (days) => {
-    //     let sum = 0;
-    //     let ans = 0;
-    //     for (let i = 0; i < arr.length; i++) {
-    //         if (days >= arr[i]) {
-    //             sum = sum + 1;
-    //             if (sum % k === 0) {
-    //                 ans = ans + sum;
-    //                 sum = 0;
-    //             }
-    //         } else {
-    //             sum = 0;
-    //         }
-    //     }
-    //     console.log(ans, "ans")
-    //     return ans;
-    // }
+    let low = 0;
+    let high = Math.max(...bloomDay);
 
-    // // let flowers = calculateFlowers(9);
-    // let result = high;
+    function isValid(day) {
 
-    // while (low <= high) {
-    //     let mid = low + Math.floor((high - low) / 2);
-    //     let flowers = calculateFlowers(mid);
-    //     if (flowers >= totalFlowersReq) {
-    //         high = mid - 1;
-    //         result = Math.min(result, mid)
-    //     } else {
-    //         low = mid + 1;
-    //     }
-    // }
-    // return result
+        let count = 0;
+        let bouquets = 0;
+        for (let i = 0; i < bloomDay.length; i++) {
 
-    // Approach 2 -> by calculating bouquets
-
-    // invalid case
-    let totalFlowersReq = m * k
-    if (arr.length < totalFlowersReq) return -1;
-
-    // bloomDay = [7,7,7,7,12,7,7], m = 2, k = 3
-
-    // low = 7;
-    // high = 12;
-    // mid = 12
-    // 1 + 1 + 1 + 1 + 1 + 1 + 1
-    let low = Math.min(...arr);
-    let high = Math.max(...arr);
-
-    const calculateBouquets = (days) => {
-        let flowers = 0;
-        let bouquet = 0;
-
-        for (let i = 0; i < arr.length; i++) {
-            if (days >= arr[i]) {
-                flowers++;
-
-                if (flowers === k) {
-                    bouquet++;
-                    flowers = 0;
+            if (bloomDay[i] <= day) {
+                count++;
+                if (count === k) {
+                    bouquets++;
+                    count = 0;
                 }
             } else {
-                flowers = 0;
+                count = 0;
             }
         }
+        // console.log(bouquets, day, 'debug')
+        if (bouquets >= m) {
+            return true;
+        } else {
+            return false;
+        }
 
-        return bouquet;
+
     }
-
-    // let flowers = calculateFlowers(9);
-    let result = high;
 
     while (low <= high) {
         let mid = low + Math.floor((high - low) / 2);
-        let bouquets = calculateBouquets(mid);
-        if (bouquets >= m) {
+        let valid = isValid(mid);
+        if (valid) {
+            ans = Math.min(ans, mid);
             high = mid - 1;
-            result = Math.min(result, mid)
         } else {
-            low = mid + 1;
+            low = mid + 1
         }
     }
-    return result
 
+    return ans;
 };
