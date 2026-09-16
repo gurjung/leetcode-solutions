55d48a797d44cb8ee5a0edb6fd6532197b2d2ddf
gurjung <88628578+gurjung@users.noreply.github.com>
2026-09-16 14:49:37 +0530

    Time: 3 ms (12.78%), Space: 58.4 MB (69.32%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.js
+++ 0733-flood-fill/0733-flood-fill.js
@@ -6,63 +6,52 @@
  * @return {number[][]}
  */
 var floodFill = function (image, sr, sc, color) {
-
-
-    let m = image.length;
-    let n = image[0].length;
-
-
-    let visitedArr = [];
-
-    for (let i = 0; i <= m; i++) {
-        visitedArr[i] = []
-        for (let j = 0; j <= n; j++) {
-            visitedArr[i][j] = false
-        }
-    }
+    let clone = image.map((row) => [...row]);
 
     let dx = [1, -1, 0, 0];
     let dy = [0, 0, -1, 1];
 
-    function isValid(i, j) {
-        if (i >= m || i < 0 || j >= n || j < 0) {
-            return false
+    let m = image.length;
+    let n = image[0].length;
+
+    let visitedArr = [];
+
+    for (let i = 0; i < m; i++) {
+        visitedArr[i] = []
+        for (let j = 0; j < n; j++) {
+            visitedArr[i][j] = false;
         }
-        return true;
     }
 
-    function recur(r, c) {
-        // base condition
+    function isValid(i, j) {
+        if (i < 0 || i >= m || j < 0 || j >= n) {
+            return false
+        }
 
+        return true
+    }
+
+    function recur(r, c, startColor) {
         visitedArr[r][c] = true;
-        clone[r][c] = color;
+        image[r][c] = color;
 
         for (let k = 0; k < 4; k++) {
             let row = r + dx[k];
             let col = c + dy[k];
-            if (isValid(row, col) && !visitedArr[row][col]) {
-                if (clone[row][col] === image[sr][sc]) {
-                    recur(row, col)
-                }
 
+            if (isValid(row, col) && !visitedArr[row][col] && clone[row][col] === startColor) {
+                recur(row, col, startColor)
             }
         }
     }
 
-    const clone = image.map(row => [...row]);
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (i === sr && j === sc) {
+                recur(i, j, clone[sr][sc])
+            }
+        }
+    }
 
-    // for (let i = 0; i < m; i++) {
-    //     for (let j = 0; j < n; j++) {
-    //         if (i === sr && j === sc) {
-    //             if (!visitedArr[i][j]) {
-    //                 recur(i, j)
-    //             }
-
-    //         }
-    //     }
-    // }
-    recur(sr, sc)
-
-    return clone
-
+    return image
 };
