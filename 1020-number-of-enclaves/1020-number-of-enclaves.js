96c3e6ee4288f3ced4962b76a73953fe727752ac
gurjung <88628578+gurjung@users.noreply.github.com>
2026-09-18 22:49:00 +0530

    Time: 26 ms (43.64%), Space: 71.9 MB (24.24%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.js
+++ 1020-number-of-enclaves/1020-number-of-enclaves.js
@@ -6,22 +6,22 @@ var numEnclaves = function (grid) {
     let m = grid.length;
     let n = grid[0].length;
 
-    // 0 = sea cell
-    // 1 = land cell
-    let count = 0;
     let ans = 0;
-    let isEnclave = false;
-
-    let dx = [1, -1, 0, 0]; // btlr
-    let dy = [0, 0, -1, 1];
+    let count = 0;
+    let isEnclave = true;
 
     let visitedArr = [];
+
     for (let i = 0; i < m; i++) {
-        visitedArr[i] = []
+        visitedArr[i] = [];
         for (let j = 0; j < n; j++) {
-            visitedArr[i][j] = false
+            visitedArr[i][j] = false;
         }
     }
+
+    let dx = [1, -1, 0, 0];
+
+    let dy = [0, 0, -1, 1];
 
     function isValid(i, j) {
         if (i >= m || i < 0 || j >= n || j < 0) {
@@ -32,9 +32,10 @@ var numEnclaves = function (grid) {
     }
 
     function isOnBoundary(i, j) {
-        if (i === 0 || i === m - 1 || j === 0 || j === n - 1) {
-            return true
+        if (i === m - 1 || i === 0 || j === n - 1 || j === 0) {
+            return true;
         }
+
         return false;
     }
 
@@ -51,24 +52,26 @@ var numEnclaves = function (grid) {
             let col = c + dy[k];
 
             if (isValid(row, col) && grid[row][col] === 1 && !visitedArr[row][col]) {
-                recur(row, col);
+                recur(row, col)
             }
         }
+
     }
+
 
     for (let i = 0; i < m; i++) {
         for (let j = 0; j < n; j++) {
-            if (grid[i][j] === 1 && !visitedArr[i][j]) {
-                count = 0;
+            if (!visitedArr[i][j] && grid[i][j] === 1) {
                 isEnclave = true;
-                recur(i, j);
-
+                count = 0;
+                recur(i, j)
                 if (isEnclave) {
                     ans = ans + count
                 }
             }
+
         }
     }
 
-    return ans
+    return ans;
 };
