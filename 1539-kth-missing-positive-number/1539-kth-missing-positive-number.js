ce2845d8cb946f15e0268847cefc5c312c177341
gurjung <88628578+gurjung@users.noreply.github.com>
2026-09-19 16:13:02 +0530

    Time: 106 ms (5.39%), Space: 54 MB (48.34%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.js
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.js
@@ -5,17 +5,48 @@
  */
 var findKthPositive = function (arr, k) {
     // brute force
-    let num = 1;
+    // let num = 1;
 
-    while (k > 0) {
-        if (!arr.includes(num)) {
-            k--;
+    // while (k > 0) {
+    //     if (!arr.includes(num)) {
+    //         k--;
+    //     }
+
+    //     if (k === 0) {
+    //         return num;
+    //     }
+
+    //     num++;
+    // }
+
+    // approach 2 -> BS
+
+    let low = 1;
+    let high = arr.length + k;
+
+    function countMissing(num) {
+        let c = 0;
+        for (let i = 1; i <= num; i++) {
+            if (!arr.includes(i)) {
+                c++;
+            }
         }
 
-        if (k === 0) {
-            return num;
-        }
-
-        num++;
+        return c;
     }
+
+    while (low <= high) {
+        let mid = low + Math.floor((high - low) / 2);
+        let count = countMissing(mid);
+        if (count < k) {
+            low = mid + 1;
+        } else {
+            ans = mid;
+            high = mid - 1;
+        }
+    }
+
+    return ans
+
+
 };
