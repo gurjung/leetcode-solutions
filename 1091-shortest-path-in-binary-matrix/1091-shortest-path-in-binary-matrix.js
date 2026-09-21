f3db61118d6b9035628427b9f5053801e95b4894
gurjung <88628578+gurjung@users.noreply.github.com>
2026-09-21 13:39:37 +0530

    Time: 25 ms (96.49%), Space: 60.7 MB (98.92%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.js
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.js
@@ -3,57 +3,50 @@
  * @return {number}
  */
 var shortestPathBinaryMatrix = function (grid) {
-    // bfs -> because all edges have same cost
-
     let m = grid.length;
     let n = grid[0].length;
 
     if (grid[0][0] === 1) return -1;
     if (grid[m - 1][n - 1] === 1) return -1;
 
-    // let result = [];
+    let dx = [1, -1, 0, 0, -1, 1, -1, 1]; // diagonal included
+    let dy = [0, 0, -1, 1, -1, 1, 1, -1]; // diagonal included
+
     let visitedArr = [];
 
     for (let i = 0; i < m; i++) {
-        // result[i] = [];
-        visitedArr[i] = [];
+        visitedArr[i] = []
         for (let j = 0; j < n; j++) {
-            // result[i][j] = -1;
             visitedArr[i][j] = false;
         }
     }
 
-    let length = 1;
-
-    let dx = [1, -1, 0, 0, -1, -1, 1, 1]; // btlr, ul, ur, bl, br
-    let dy = [0, 0, -1, 1, -1, 1, -1, 1];
-
-    let q = [{ r: 0, c: 0, dist: 1 }]
-
-    visitedArr[0][0] = true;
-
     function isValid(i, j) {
-        if (i >= m || i < 0 || j >= n || j < 0) {
+        if (i < 0 || i >= m || j < 0 || j >= n) {
             return false;
         }
+
         return true;
     }
+
+    let q = [{ r: 0, c: 0, dist: 1 }];
+    visitedArr[0][0] = true;
 
     while (q.length) {
         let curr = q.shift();
         let { r, c, dist } = curr;
 
+
         if (r === m - 1 && c === n - 1) {
-            return dist;
+            return dist
         }
-        // result[r][c] = dist;
 
         for (let k = 0; k < 8; k++) {
             let row = r + dx[k];
             let col = c + dy[k];
 
             if (isValid(row, col) && !visitedArr[row][col] && grid[row][col] === 0) {
-                visitedArr[row][col] = true
+                visitedArr[row][col] = true;
                 q.push({ r: row, c: col, dist: dist + 1 })
             }
         }
