9be236ed7ef13164e68cf5c153d2dcd48e1ed019
gurjung <88628578+gurjung@users.noreply.github.com>
2026-09-22 15:09:57 +0530

    Time: 2 ms (86.64%), Space: 59.8 MB (37.03%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.js
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.js
@@ -4,30 +4,50 @@
  * @return {number}
  */
 var findMedianSortedArrays = function (nums1, nums2) {
-    let m = nums1.length;
-    let n = nums2.length;
+    // let m = nums1.length;
+    // let n = nums2.length;
 
-    let mergedArr = [...nums1, ...nums2];
-    mergedArr.sort((a, b) => a - b);
+    // let mergedArr = [...nums1, ...nums2];
+    // mergedArr.sort((a, b) => a - b);
 
-    let low = 0;
-    let high = mergedArr.length - 1;
-    let isEven = mergedArr.length % 2 === 0
-    let ans = 0;
-    console.log(mergedArr, 'debug')
-    while (low <= high) {
-        let mid = low + Math.floor((high - low) / 2);
-        if (!isEven) {
-            ans = mergedArr[mid];
-            break;
+    // let low = 0;
+    // let high = mergedArr.length - 1;
+    // let isEven = mergedArr.length % 2 === 0
+    // let ans = 0;
+    // console.log(mergedArr, 'debug')
+    // while (low <= high) {
+    //     let mid = low + Math.floor((high - low) / 2);
+    //     if (!isEven) {
+    //         ans = mergedArr[mid];
+    //         break;
+    //     } else {
+    //         ans = ((mergedArr[mid] + mergedArr[mid + 1]) / 2)
+    //         break
+    //     }
+
+
+    // }
+
+    // return ans;
+
+    let merged = [];
+    let p1 = 0, p2 = 0;
+
+    while (p1 < nums1.length && p2 < nums2.length) {
+        if (nums1[p1] <= nums2[p2]) {
+            merged.push(nums1[p1++]);
         } else {
-            ans = ((mergedArr[mid] + mergedArr[mid + 1]) / 2)
-            break
+            merged.push(nums2[p2++]);
         }
-
-
     }
+    while (p1 < nums1.length) merged.push(nums1[p1++]);
+    while (p2 < nums2.length) merged.push(nums2[p2++]);
 
-    return ans;
+    // Direct O(1) index lookup - no search needed
+    const mid = Math.floor(merged.length / 2);
+    if (merged.length % 2 === 1) {
+        return merged[mid];
+    }
+    return (merged[mid - 1] + merged[mid]) / 2;
 
 };
